7d54db8434e28c57f3cd9e5995babb2b600a9013
Leewansze <1226186467@qq.com>
2020-07-24 16:43:38 +0800

    [ParkingBoy][Alice]feature:pass should_return_parking_ticket_when_park_car_given_a_car

---
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -0,0 +1,8 @@
+package com.oocl.cultivation;
+
+public class ParkingBoy {
+
+    public Ticket parkCar(String carId) {
+        return new Ticket(carId);
+    }
+}
---
+++ src/main/java/com/oocl/cultivation/Ticket.java
@@ -0,0 +1,13 @@
+package com.oocl.cultivation;
+
+public class Ticket {
+    private String carId;
+
+    public Ticket(String carId) {
+        this.carId = carId;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+}
