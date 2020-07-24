22fdd01a1884d13b97370f3ad3e999c2c26b55d2
Leewansze <1226186467@qq.com>
2020-07-24 16:42:25 +0800

    [ParkingBoy][Alice]test: should_return_parking_ticket_when_park_car_given_a_car

---
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFactsTest.java
@@ -0,0 +1,22 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.ParkingBoy;
+import com.oocl.cultivation.Ticket;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ParkingBoyFactsTest {
+    @Test
+    void should_return_parking_ticket_when_park_car_given_a_car() {
+        //given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        String carId = "01";
+
+        //when
+        Ticket ticket = parkingBoy.parkCar(carId);
+
+        //then
+        assertEquals("01", ticket.getCarId());
+    }
+}
