525d211b7ab31d566af658c13461e44217acb0a9
Leewansze <1226186467@qq.com>
2020-07-25 17:09:45 +0800

    [ParkingBoy][Alice] test: should_return_a_car_when_fetch_car_given_parking_ticket_parkingBoy_a_car_in_parkingLot

--- src/test/java/com/oocl/cultivation/test/ParkingBoyFactsTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFactsTest.java
@@ -1,37 +1,41 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.Car;
 import com.oocl.cultivation.ParkingBoy;
 import com.oocl.cultivation.Ticket;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingBoyFactsTest {
     @Test
     void should_return_parking_ticket_when_park_car_given_a_car() {
         //given
         ParkingBoy parkingBoy = new ParkingBoy();
-        String carId = "01";
+        Car car = new Car();
 
         //when
-        Ticket ticket = parkingBoy.parkCar(carId);
+        Ticket ticket = parkingBoy.parkCar(car);
 
         //then
-        assertEquals("01", ticket.getCarId());
+        assertNotNull(ticket);
     }
 
     @Test
     void should_return_a_car_when_fetch_car_given_parking_ticket_parkingBoy_a_car_in_parkingLot() {
         //given
         ParkingBoy parkingBoy = new ParkingBoy();
-        Ticket ticket = new Ticket("01");
+//        Ticket ticket = new Ticket();
+        Car car = new Car();
 
         //when
-        String carId = parkingBoy.fetchCar(ticket.getCarId());
+        Ticket ticket = parkingBoy.parkCar(car);
+        Car carGetFromParkingBoy = parkingBoy.fetchCar(ticket);
 
         //then
-        assertEquals("car", carId);
-
-
+        assertNotNull(ticket);
+        assertEquals(car, carGetFromParkingBoy);
     }
+
+
 }
