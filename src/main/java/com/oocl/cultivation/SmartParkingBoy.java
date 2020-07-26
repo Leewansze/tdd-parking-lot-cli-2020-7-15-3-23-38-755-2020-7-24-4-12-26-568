05b7d26f01c84bb7572653a94b06504a3773c61d
Leewansze <1226186467@qq.com>
2020-07-26 14:42:09 +0800

    [ParkingBoy][Alice] test: should_return_a_tikcet_when_park_car_to_more_position_parkingLot_given_a_smart_parkingBoy_a_car_3_parkingLots

---
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -0,0 +1,12 @@
+package com.oocl.cultivation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmartParkingBoy {
+    private List<ParkingLot> parkingLots = new ArrayList<>();
+
+    public Ticket parkCarInMorePositionParkingLot(Car car) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
@@ -0,0 +1,31 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.SmartParkingBoy;
+import com.oocl.cultivation.Ticket;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SmartParkingBoyTest {
+    @Test
+    void should_return_a_tikcet_when_park_car_to_more_position_parkingLot_given_a_smart_parkingBoy_a_car_3_parkingLots() {
+        //given
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        for (int index = 0; index < 2; index++){
+            parkingLots.add(new ParkingLot());
+        }
+        parkingLots.get(0).parkCar(new Car());
+
+        //when
+        Ticket ticket = smartParkingBoy.parkCarInMorePositionParkingLot(new Car());
+
+        //then
+        assertNotNull(ticket);
+    }
+}
