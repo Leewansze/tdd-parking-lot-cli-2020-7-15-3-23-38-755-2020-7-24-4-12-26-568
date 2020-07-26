6c1b4a7ef1ea4079b1c93862132ce3da81329d65
Leewansze <1226186467@qq.com>
2020-07-26 18:01:00 +0800

    [ParkingBoy][Alice] test: should_return_a_ticket_and_right_parkingLot_when_park_in_Larger_avail_position_parkingLot_given_supersmartboy_2_parkingLot_a_car

---
+++ src/main/java/com/oocl/cultivation/SuperSmartBoy.java
@@ -0,0 +1,8 @@
+package com.oocl.cultivation;
+
+public class SuperSmartBoy {
+
+    public Ticket parkCarInLargerAvailPositionRateLot(Car car) {
+        return null;
+    }
+}
--- src/test/java/com/oocl/cultivation/test/SuperSmartBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartBoyTest.java
@@ -1,17 +1,30 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SuperSmartBoyTest {
     @Test
-    void should_return_a_ticket_when_park_car_in__given_() {
+    void should_return_a_ticket_and_right_parkingLot_when_park_in_Larger_avail_position_parkingLot_given_supersmartboy_2_parkingLot_a_car() {
         //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        SuperSmartBoy superSmartBoy = new SuperSmartBoy();
+        for(int index = 0; index < 2; index++){
+            parkingLots.add(new ParkingLot());
+        }
+        parkingLots.get(0).parkCar(new Car());
 
         //when
-
+        Ticket ticket = superSmartBoy.parkCarInLargerAvailPositionRateLot(new Car());
 
         //then
-
-
+        assertNotNull(ticket);
+        assertEquals(1, parkingLots.get(1).getParkingLot().size());
     }
 }
