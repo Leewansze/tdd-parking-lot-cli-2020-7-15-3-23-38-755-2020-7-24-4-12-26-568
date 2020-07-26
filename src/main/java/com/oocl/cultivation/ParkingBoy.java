2d87df9bce572e68ea15ec8d7c87172179a46246
Leewansze <1226186467@qq.com>
2020-07-26 11:53:42 +0800

    [ParkingBoy][Alice] test: should_return_a_ticket_when_park_fill_one_parkingLot_then_move_to_another_given_a_car_two_parkingLot

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,58 +1,16 @@
 package com.oocl.cultivation;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParkingBoy {
-    private Map<Ticket, Car> parkingBoy;
-    private Ticket ticket;
-    private int parkingLotCapacity;
+    private List<ParkingLot> parkingLots = new ArrayList<>();
 
-    public ParkingBoy() {
-        this.parkingLotCapacity = 10;
-        this.parkingBoy = new HashMap<>();
+    public ParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
     }
 
-    public Ticket parkCar(Car car) {
-        if(parkingLotCapacity > 0){
-            Ticket ticket = new Ticket();
-            this.parkingBoy.put(ticket, car);
-            parkingLotCapacity--;
-            return ticket;
-        }
+    public Ticket parkingBoyParkCar(Car car) {
         return null;
-    }
-
-    public Car fetchCar(Ticket ticket) {
-        Car car = this.parkingBoy.get(ticket);
-        Iterator iterator = parkingBoy.keySet().iterator();
-//        while (iterator.hasNext()) {
-//            Object key = iterator.next();
-//            if (ticket.equals(key)) {
-//                parkingBoy.remove(key);
-//            }
-//        }
-        this.parkingBoy.remove(ticket);
-        return car;
-    }
-
-    public String fetchCarReturnDetail(Ticket ticket) {
-        Car car = this.fetchCar(ticket);
-        if(ticket == null){
-            return "Please provide your parking ticket.";
-        }
-        if(car == null){
-            return "Unrecognized parking ticket.";
-        }
-        return car.toString();
-    }
-
-    public String parkCarReturnDetail(Car car) {
-        Ticket ticket = this.parkCar(car);
-        if(ticket == null){
-            return "Not enough position.";
-        }
-        return ticket.toString();
     }
 }
