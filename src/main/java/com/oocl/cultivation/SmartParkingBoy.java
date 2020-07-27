5ef502266aef812e947e530e06ffd76ee9e73d34
Leewansze <1226186467@qq.com>
2020-07-27 22:27:48 +0800

    [ParkingBoy][Alice] refactor: use stream instead of for loop

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -1,10 +1,12 @@
 package com.oocl.cultivation;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class SmartParkingBoy extends AbstractParkingBoy{
-    private List<ParkingLot> parkingLots = new ArrayList<>();
+public class SmartParkingBoy extends AbstractParkingBoy {
+    private List<ParkingLot> parkingLots;
 
     public SmartParkingBoy(List<ParkingLot> parkingLots) {
         this.parkingLots = parkingLots;
@@ -12,16 +14,11 @@ public class SmartParkingBoy extends AbstractParkingBoy{
 
     @Override
     public Ticket parking(Car car) {
-        ParkingLot parkingLotMax = parkingLots.get(0);
-        for(int index = 0; index < parkingLots.size(); index++){
-            if(parkingLotMax.getParkingLotCapacity() < parkingLots.get(index).getParkingLotCapacity()){
-                parkingLotMax = parkingLots.get(index);
-            }
-        }
+        ParkingLot targetParkingLot = parkingLots.stream()
+                .sorted(Comparator.comparingInt(ParkingLot::getParkingLotCapacity).reversed())
+                .collect(Collectors.toList())
+                .get(0);
 
-        Ticket ticket = parkingLotMax.parkCar(car);
-        System.out.println(parkingLots.get(1).getParkingLot().size());
-        System.out.println(parkingLots.get(0).getParkingLot().size());
-        return ticket;
+        return targetParkingLot.parkCar(car);
     }
 }
